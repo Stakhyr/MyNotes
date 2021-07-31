c29d25833306a38673c47231adcbdb766ff01eef
Stakhyr <56436513+Stakhyr@users.noreply.github.com>
2021-07-31 19:59:38 +0300

    Set up DbContext, add Note model

---
+++ MyNotes/Data/MyNotesDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyNotes.Data
+{
+    public class MyNotesDbContext:DbContext
+    {
+        public MyNotesDbContext(DbContextOptions<MyNotesDbContext> options) : base(options) { }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+}
